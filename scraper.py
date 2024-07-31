242ffce78d125ea5be09d5c820fa604567626908
Giovanni Pirrotta <giovanni.pirrotta@gmail.com>
2024-07-31 20:45:06 +0200

    Initial commit

---
+++ scraper.py
@@ -0,0 +1,99 @@
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.document_loaders import DataFrameLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
+import requests
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+
+
+PREFIX = 'https://www.unime.it'
+dataset = []
+
+## SCRAPE PRIMA TIPOLOGIA PAGINA FAQ (DOMANDA/RISPOSTA)
+url = 'https://www.unime.it/didattica/immatricolazioni-e-iscrizioni/faq-immatricolazioni'
+
+
+
+page = requests.get(url, verify=False)
+print(url)
+soup = BeautifulSoup(page.text)
+title = soup.find('h2', {'class' : 'title-page__title'}).text.strip()
+for item in soup.find_all('div', {'class' : 'accordion-item'}):
+    question =  item.find('button', {'class' : 'accordion-button'}).text.strip()
+    answer = item.find('div', {'class':'field--name-field-testo-paragrafo'}).text.strip()
+    body =  question + '\n' + answer
+    attachments = item.find('div', {'class':'paragraph__allegati'})
+    if attachments:
+        body = body + '\nLink relativi:\n' 
+        for a in attachments.find_all('a'):
+            body = body + a.text + f" ({PREFIX}{a.get('href')})\n"
+    dataset.append({'url':url, 'title': title, 'body':body})
+
+time.sleep(3)    
+
+## SCRAPE SECONDA TIPOLOGIA PAGINA FAQ (PAGINA INTERA)
+
+page_links = ['https://www.unime.it/didattica/immatricolazioni-e-iscrizioni/corsi-liberi-aa-202324-modalita-e-termini',
+              'https://www.unime.it/didattica/immatricolazioni-e-iscrizioni/contemporanea-iscrizione-due-corsi-di-istruzione-superiore',
+              'https://www.unime.it/didattica/immatricolazioni-e-iscrizioni/corsi-numero-chiuso',
+              'https://www.unime.it/didattica/immatricolazioni-e-iscrizioni/corsi-singoli',
+              'https://www.unime.it/didattica/immatricolazioni-e-iscrizioni/i-documenti-necessari',
+              'https://www.unime.it/didattica/immatricolazioni-e-iscrizioni/i-vari-tipi-di-corsi-di-laurea',
+              'https://www.unime.it/didattica/immatricolazioni-e-iscrizioni/il-sistema-pago-par',
+              'https://www.unime.it/didattica/immatricolazioni-e-iscrizioni/immatricolazioni-personale-pa',
+              'https://www.unime.it/didattica/immatricolazioni-e-iscrizioni/info-point',
+              'https://www.unime.it/didattica/immatricolazioni-e-iscrizioni/live-chat-studenti',
+              'https://www.unime.it/didattica/immatricolazioni-e-iscrizioni/ricongiunzione-eo-ripresa-carriera-ex-studenti',
+              'https://www.unime.it/didattica/immatricolazioni-e-iscrizioni/sedi-didattiche-distaccate',
+              'https://www.unime.it/didattica/immatricolazioni-e-iscrizioni/tasse-ed-esenzioni',
+              'https://www.unime.it/didattica/immatricolazioni-e-iscrizioni/tempo-parziale',
+              'https://www.unime.it/didattica/immatricolazioni-e-iscrizioni/trasferimenti'
+              ]
+
+for url in page_links:
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.text)
+    title = soup.find('h2', {'class' : 'title-page__title'}).text.strip()
+    print(f"Parsing page: {title}")
+    body = soup.find('div', {'class' : 'block-field-blocknodepagefield-contenuto'}).get_text(separator = '\n', strip = True)
+    for attachments in soup.find_all('div', {'class':'paragraph__allegati'}):
+        if attachments:
+            body = body + '\nLink relativi:\n' 
+            for a in attachments.find_all('a'):
+                body = body + a.text + f" ({PREFIX}{a.get('href')})\n"
+    dataset.append({'url':url, 'title': title, 'body':body})
+    time.sleep(3)
+    
+    
+df = pd.DataFrame(dataset)
+df['body'] = df['body'].str.replace('Document', ' ')
+df['body'] = df['body'].str.replace("\n",' ').str.replace('  ',' ').str.strip()
+
+df.to_csv('./data/dataset.csv', index=False)
+
+
+chunks = DataFrameLoader(
+        dataset, page_content_column="body"
+    ).load_and_split(
+        text_splitter=RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=0, length_function=len
+        )
+    )
+    
+for doc in chunks:
+    title = doc.metadata["title"]
+    content = doc.page_content
+    url = doc.metadata["url"]
+    final_content = f"TITLE: {title}\BODY: {content}\nURL: {url}"
+    doc.page_content = final_content
+    
+  
+load_dotenv()
+embeddings = OpenAIEmbeddings(model='text-embedding-ada-002') 
+
+vectorstore = FAISS.from_documents(chunks, embeddings)
+vectorstore.save_local("./db")
---
+++ start.py
@@ -0,0 +1,126 @@
+from dotenv import load_dotenv
+import pandas as pd
+from langchain.embeddings import OpenAIEmbeddings 
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+    PromptTemplate,
+    MessagesPlaceholder
+)
+import streamlit as st
+
+load_dotenv()
+
+
+def get_conversation_chain(vector_store:FAISS, system_message:str, human_message:str) -> ConversationalRetrievalChain:
+    
+     # questo è il cuore del sistema. Qui viene invocato il modello di linguaggio GPT
+    llm = ChatOpenAI(model="gpt-4", temperature=0)
+    # Qui è dove LangChain svolge il ruolo di direttore d'orchestra, collegando tutte le strumenti del sistema
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm, # questo è il modello GPT
+        retriever=st.session_state.vector_store.as_retriever(), # quà viene definito il database vettoriale
+        return_source_documents=True,
+        verbose=True,
+        memory=st.session_state.memory,
+        get_chat_history=lambda h : h,
+        #condense_question_prompt=PromptTemplate.from_template(
+        #    ('Questa è la precedente domanda {question}), questa è la precedente risposta ({chat_history}).')
+        #),
+        combine_docs_chain_kwargs={ # qui viene specificato il prompt come unione di più parti tenute insieme tramite un template
+            "prompt": ChatPromptTemplate.from_messages(
+                [
+                    system_message,
+                    MessagesPlaceholder(variable_name="chat_history"),
+                    human_message,
+                ]
+                ),
+            },
+        )
+    
+    return conversation_chain
+
+
+
+def setup_chat_header():
+    st.set_page_config(
+        page_title="Documentation Chatbot",
+        page_icon=":school:",
+    )
+
+    st.image("https://archivio.unime.it/sites/default/files/7f7ebabb21b60312954b1d1ecc3e1eeddab2926e/409x205xloco,P20unime,P20orizzontale.jpg.pagespeed.ic.nIu0lfMiM1.webp") 
+    st.title("UNIME Chatbot")
+    st.subheader("Quando l'AI entra in Ateneo")
+    st.markdown(
+        """
+        Questo chatbot è stato creato per rispondere alle domande riguardanti l'immatricolazione
+        e l'iscrizione all'Università di Messina.
+        Poni una domanda e il chatbot ti risponderà nel migliore dei modi.
+        """
+    )
+
+def main():
+    if 'vector_store' not in st.session_state:
+        vector_store = FAISS.load_local("./db", OpenAIEmbeddings(), allow_dangerous_deserialization=True)
+        st.session_state.vector_store = vector_store
+    if "memory" not in st.session_state:
+        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, output_key='answer')
+        st.session_state.memory = memory
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+        
+        
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None  
+
+    # ISTRUZIONI PROMPT
+    system_message_prompt = SystemMessagePromptTemplate.from_template(
+        """
+        Sei un chatbot e il tuo compito è rispondere a domande sull'immatricolazione e iscrizione
+        dei corsi di laurea all'Università di Messina.
+        Ad ogni domanda devi sempre rispondere con la risposta più rilevante e più accurata.
+        Non rispondere mai con una nuova domanda.
+        Non rispondere a domande non inerenti l'Università di Messina. 
+        Se la domanda è in italiano rispondi in italiano. Se la domanda è in inglese rispondi in inglese.
+        Data una domanda dovresti rispondere con le informazioni più rilevanti in base al seguente contesto: \n
+        {context}
+        """
+    )
+    
+    human_message_prompt = HumanMessagePromptTemplate.from_template("{question}")
+       
+    
+    # SETUP STREAMLIT
+    setup_chat_header()     
+    user_question = st.text_input("Cosa vuoi chiedere?")
+    with st.spinner("Sto elaborando la risposta..."):
+        if user_question:
+            response = st.session_state.conversation({"question": user_question })
+            st.session_state.chat_history = response["chat_history"]
+
+            human_style = "background-color: #e6f7ff; border-radius: 10px; padding: 10px;"
+            chatbot_style = "background-color: #f9f9f9; border-radius: 10px; padding: 10px;"
+            
+            for i, message in enumerate(st.session_state.chat_history):
+                if i % 2 == 0:
+                    st.markdown(
+                        f"<p style='text-align: right;'><b>Utente</b></p><p style='text-align: right;{human_style}'> <i>{message.content}</i> </p>",
+                        unsafe_allow_html=True,
+                    )
+                else:
+                    st.markdown(
+                        f"<p style='text-align: left;'><b>Chatbot</b></p> <p style='text-align: left;{chatbot_style}'> <i>{message.content}</i> </p>",
+                        unsafe_allow_html=True,
+                    )
+            st.session_state.input = ""  
+    
+    st.session_state.conversation = get_conversation_chain(st.session_state.vector_store, system_message_prompt, human_message_prompt)
+    
+    
+if __name__ == "__main__":
+    main()
